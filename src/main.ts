bf528e39d1f05d6049803224bf273100a3d2967e
Owoyemi Olusola <olusola.owoyemi@cyberspace.net.ng>
2026-02-20 18:00:34 +0100

    resolve cors issue

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,14 @@ import { RequestContextInterceptor } from './common/interceptor/requestContext';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
-
+  app.enableCors({
+    origin: ['https://procuree-fe.vercel.app', 'http://localhost:3000'],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // keep only if frontend uses cookies/withCredentials
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
